1353fd982ea2cd617f1956a7b9247b14d62046ad
KAMUZUKI <1437487442@qq.com>
2022-11-13 21:28:01 +0800

    savepoint:to conformity editor and form

--- vue3_test/src/components/EditFramework.vue
+++ vue3_test/src/components/EditFramework.vue
@@ -3,7 +3,12 @@
         <a-layout-content>
             <a-space :size="small" direction="horizontal" align="start">
                 <a-col>
-                    <MavonEditor ref="getContent" :content="formState.content"></MavonEditor>
+                    <div style="border: 1px solid #ccc;text-align:left;">
+                        <Toolbar style="border-bottom: 1px solid #ccc" :editor="editorRef"
+                            :defaultConfig="toolbarConfig" :mode="mode" />
+                        <Editor style="height: 500px; overflow-y: hidden;" v-model="valueHtml"
+                            :defaultConfig="editorConfig" :mode="mode" @onCreated="handleCreated" />
+                    </div>
                 </a-col>
                 <a-col style="background-color:#fff;" flex="fix">
                     <a-card style="width: 300px;border-radius: 20px;" hoverable>
@@ -23,12 +28,14 @@
                                 <a-input v-model:value="formState['avatar']" />
                             </a-form-item>
                             <a-form-item :name="categorys" label="栏目选择">
-                                <a-select showSearch v-model:categoryOptions="categoryOptions" mode="multiple" style="width: 100%"
-                                    placeholder="请选择栏目" :options="categorys" @change="handleChange"></a-select>
+                                <a-select showSearch v-model:categoryOptions="categoryOptions" mode="multiple"
+                                    style="width: 100%" placeholder="请选择栏目" :options="categorys" @change="handleChange">
+                                </a-select>
                             </a-form-item>
                             <a-form-item :name="keywords" label="关键词">
-                                <a-select showSearch v-model:keywordOptions="keywordOptions" mode="multiple" style="width: 100%"
-                                    placeholder="请选择关键词" :options="keywords" @change="handleChange"></a-select>
+                                <a-select showSearch v-model:keywordOptions="keywordOptions" mode="multiple"
+                                    style="width: 100%" placeholder="请选择关键词" :options="keywords" @change="handleChange">
+                                </a-select>
                             </a-form-item>
                             <a-button @click="onFinish" type="primary" shape="round" :size="size">
                                 <template #icon>
@@ -42,12 +49,11 @@
             </a-space>
         </a-layout-content>
     </a-layout>
-    <NotificationComponent ref="openNotification"/>
+    <NotificationComponent ref="openNotification" />
 </template>
 
 <script>
-import { defineComponent, ref,toRef, onMounted, createVNode } from 'vue';
-import MavonEditor from './MavonEditor/MavonEditor.vue';
+import { defineComponent, ref, toRef, onMounted, createVNode } from 'vue';
 import { ExclamationCircleOutlined } from '@ant-design/icons-vue';
 import NotificationComponent from './tools/NotificationComponent.vue';
 import { Modal } from 'ant-design-vue';
@@ -55,6 +61,11 @@ import { useRouter } from 'vue-router';
 import { useRoute } from 'vue-router'
 import { useStore } from 'vuex' // 引入useStore 方法
 
+import '@wangeditor/editor/dist/css/style.css' // 引入 css
+
+import { onBeforeUnmount, shallowRef } from 'vue'
+import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
+import axios from 'axios';
 export default defineComponent({
     name: 'EditFramework',
     setup() {
@@ -78,8 +89,8 @@ export default defineComponent({
             createTime: '',
             colCnt: [],
         });
-        
-        const judgeMode = ()=>{
+
+        const judgeMode = () => {
             const editInfo = route.query.articleId ?? '0'
             if (editInfo !== '0') {
                 //TODO:通过articleId获取文章详情
@@ -97,6 +108,7 @@ export default defineComponent({
                 }
                 catagoryOptions.value = toRef(formState.value.categorys)
                 keywordOptions.value = toRef(formState.value.keywords)
+
             }
         }
 
@@ -106,6 +118,9 @@ export default defineComponent({
                 router.push('/')
                 return
             }
+            setTimeout(() => {
+                valueHtml.value = formState.value.content ?? ''
+            }, 500)
             judgeMode()
         })
 
@@ -155,11 +170,49 @@ export default defineComponent({
             console.log(`selected ${value}`);
         };
 
-        const onFinish = values => {
-            openNotification.value.openNotificationWithIcon('success','表单提交', '提交成功');
-            formState.value.content = getContent.value.getContent
-            console.log('Success:', values);
+        const onFinish = () => {
+            let params = new URLSearchParams();
+            params.append("id", formState.value.id);
+            params.append("author", formState.value.author);
+            params.append("title", formState.value.title);
+            params.append("avatar", formState.value.avatar);
+            params.append("description", formState.value.description);
+            params.append("content", formState.value.content);
+            params.append("keywords", formState.value.keywords);
+            params.append("categorys", formState.value.categorys);
+            params.append("createTime", formState.value.createTime);
+            axios.post("article.action", params).then(result => {
+                let json = result.data;
+                if (json.code == 1) {
+                    openNotification.value.openNotificationWithIcon('成功', '表单提交', '提交成功')
+                } else {
+                    openNotification.value.openNotificationWithIcon('失败', '表单提交', '提交失败')
+                }
+            })
         };
+
+        //===============mavon-editor================
+        // 编辑器实例，必须用 shallowRef
+        const editorRef = shallowRef()
+
+        // 内容 HTML
+        const valueHtml = ref('')
+
+        // 模拟 ajax 异步获取内容
+
+        const toolbarConfig = {}
+        const editorConfig = { placeholder: '请输入内容...' }
+
+        // 组件销毁时，也及时销毁编辑器
+        onBeforeUnmount(() => {
+            const editor = editorRef.value
+            if (editor == null) return
+            editor.destroy()
+        })
+
+        const handleCreated = (editor) => {
+            editorRef.value = editor // 记录 editor 实例，重要！
+        }
 
         return {
             formState,
@@ -169,13 +222,20 @@ export default defineComponent({
             openNotification,
             catagoryOptions,
             keywordOptions,
+            editorRef,
+            valueHtml,
+            mode: 'default', // 或 'simple'
+            toolbarConfig,
+            editorConfig,
+            handleCreated,
             onFinish,
             handleChange,
         };
     },
     components: {
-        MavonEditor,
-        NotificationComponent
+        NotificationComponent,
+        Editor,
+        Toolbar
     },
     props: {
         openNotificationWithIcon: {
